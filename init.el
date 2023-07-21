9f3fbf68cea85996441fb4496c76fbb67339db33
julianleelinker <julianlee@linkerNetworks.com>
2023-07-22 01:08:38 +0800

    Get elpaca working

--- init.el
+++ init.el
@@ -6,6 +6,10 @@
 ;; what variables specify), the help system can provide.
 
 
+;; TODO Review yt emacs element about elpaca
+;; TODO Try fresh install the config
+
+
 (setq user-full-name "Julian Lee"
       user-mail-address "pcs155251@gmail.com")
 
@@ -44,6 +48,97 @@
 ;; Better line warp setting without change line within same word
 (global-visual-line-mode nil)
 
+;; elpaca package management
+(defvar elpaca-installer-version 0.5)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil
+                              :files (:defaults (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                 ((zerop (call-process "git" nil buffer t "clone"
+                                       (plist-get order :repo) repo)))
+                 ((zerop (call-process "git" nil buffer t "checkout"
+                                       (or (plist-get order :ref) "--"))))
+                 (emacs (concat invocation-directory invocation-name))
+                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                 ((require 'elpaca))
+                 ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+
+;; Install use-package support
+(elpaca elpaca-use-package
+  ;; Enable :elpaca use-package keyword.
+  (elpaca-use-package-mode)
+  ;; Assume :elpaca t unless otherwise specified.
+  (setq elpaca-use-package-by-default t))
+
+;; Block until current queue processed.
+(elpaca-wait)
+
+;;; Vim Bindings
+(use-package evil
+  :demand t
+  :bind (("<escape>" . keyboard-escape-quit))
+  :init
+  ;; allows for using cgn
+  ;; (setq evil-search-module 'evil-search)
+  (setq evil-want-keybinding nil)
+  ;; no vim insert bindings
+  ;; (setq evil-undo-system 'undo-fu)
+  :config
+  (evil-mode 1)
+  )
+
+;; vertico completion
+(use-package vertico
+  :load-path "lib/vertico"
+  :init
+  (vertico-mode)
+  ;; Enable cycling for `vertico-next' and `vertico-previous'.
+  (setq vertico-cycle t)
+  )
+
+;;When installing a package which modifies a form used at the top-level
+;;(e.g. a package which adds a use-package key word),
+;;use `elpaca-wait' to block until that package has been installed/configured.
+;;For example:
+;;(use-package general :demand t)
+;;(elpaca-wait)
+
+;; Expands to: (elpaca evil (use-package evil :demand t))
+;;(use-package evil :demand t)
+;;
+;;;;Turns off elpaca-use-package-mode current declartion
+;;;;Note this will cause the declaration to be interpreted immediately (not deferred).
+;;;;Useful for configuring built-in emacs features.
+;;(use-package emacs :elpaca nil :config (setq ring-bell-function #'ignore))
+;;
+;;;; Don't install anything. Defer execution of BODY
+;;(elpaca nil (message "deferred"))
+
+
+
 
 ;; (unless $package-archive-contents  (package-refresh-contents))
 ;;;; Add the NonGNU ELPA package archive
@@ -54,137 +149,40 @@
 ;;
 ;;
 ;; Bootsrap use-package
-(defvar emacs-dir
-  (eval-when-compile (file-truename user-emacs-directory))
-  "The path to the .emacs.d directory. Must end with a slash.")
-(push (concat emacs-dir "lib/use-package/") load-path)
-(eval-when-compile
-  (require 'use-package))
-
-;; load packages, currently use use-package, will change to elpaca
-(use-package vertico
-  :load-path "lib/vertico"
-  :init
-  (vertico-mode)
-  ;; Enable cycling for `vertico-next' and `vertico-previous'.
-  (setq vertico-cycle t)
-  )
-
-(use-package evil
-  :load-path "lib/evil"
-  :ensure t  ;; make sure the package is installed if not already
-  :init  ;; code to execute before loading the package
-  :config  ;; code to execute after loading the package
-  (evil-mode 1)  ;; enable evil-mode
-) 
+;;(defvar emacs-dir			
+;;  (eval-when-compile (file-truename user-emacs-directory))
+;;  "The path to the .emacs.d directory. Must end with a slash.")
+;;(push (concat emacs-dir "lib/use-package/") load-path)
+;;(eval-when-compile
+;;  (require 'use-package))
+;;
+;;;; load packages, currently use use-package, will change to elpaca
 
 
-;; packages
-;; Completion framework
-;; (unless (package-installed-p 'vertico)
-;;   (package-install 'vertico))
-;; 
-;; ;; Enable completion by narrowing
-;; (vertico-mode t)
-;; 
-;; ;; Improve directory navigation
-;; (with-eval-after-load 'vertico
-;;   (define-key vertico-map (kbd "RET") #'vertico-directory-enter)
-;;   (define-key vertico-map (kbd "DEL") #'vertico-directory-delete-word)
-;;   (define-key vertico-map (kbd "M-d") #'vertico-directory-delete-char))
-;; 
-;; ;;; Extended completion utilities
-;; (unless (package-installed-p 'consult)
-;;   (package-install 'consult))
-;; (global-set-key [rebind switch-to-buffer] #'consult-buffer)
-;; (global-set-key (kbd "C-c j") #'consult-line)
-;; (global-set-key (kbd "C-c i") #'consult-imenu)
-;; (setq read-buffer-completion-ignore-case t
-;;       read-file-name-completion-ignore-case t
-;;       completion-ignore-case t)
-
-;;;;; Vim Emulation
-;;(unless (package-installed-p 'evil)
-;;  (package-install 'evil))
-;;
-;;;; Enable Vim emulation
-;;(evil-mode t)
-;;
-;;;; 
-;;(unless (package-installed-p 'minion)
-;;  (package-install 'minions))
-;;
-;;(unless (package-installed-p 'doom-modeline)
-;;  (package-install 'doom-modeline))
-;;(require 'doom-modeline)
-;;(doom-modeline-mode 1)
-;;
-;;(unless (package-installed-p 'nerd-icons)
-;;  (package-install 'nerd-icons))
 
 
-;; Relative unimportant package
-;;;;; LSP Support
-;;(unless (package-installed-p 'eglot)
-;;  (package-install 'eglot))
-;;
-;;;; Enable LSP support by default in programming buffers
-;;(add-hook 'prog-mode-hook #'eglot-ensure)
-;;
-;;;;; Inline static analysis
-;;;; Enabled inline static analysis
-;;(add-hook 'prog-mode-hook #'flymake-mode)
-;;
-;;;;; Pop-up completion
-;;(unless (package-installed-p 'corfu)
-;;  (package-install 'corfu))
-;;
-;;;; Enable autocompletion by default in programming buffers
-;;(add-hook 'prog-mode-hook #'corfu-mode)
-;;
-;;;;; Git client
-;;(unless (package-installed-p 'magit)
-;;  (package-install 'magit))
-;;
-;;;; Bind the `magit-status' command to a convenient key.
-;;(global-set-key (kbd "C-c g") #'magit-status)
-;;
-;;;; Show word-granularity differences within diff hunks
-;;(setq magit-diff-refine-hunk t)
-;;
-;;;;; Indication of local VCS changes
-;;(unless (package-installed-p 'diff-hl)
-;;  (package-install 'diff-hl))
-;;
-;;;; Enable `diff-hl' support by default in programming buffers
-;;(add-hook 'prog-mode-hook #'diff-hl-mode)
-;;
-;;;;; In-Emacs Terminal Emulation
-;;(unless (package-installed-p 'eat)
-;;  (package-install 'eat))
-;;
-;;;; Close the terminal buffer when the shell terminates.
-;;(setq eat-kill-buffer-on-exit t)
-;;
-;;;; Enable mouse-support.
-;;(setq eat-enable-mouse t)
-;;
-;;
-;;
-;;
-;;
-;;
-;;;; Miscellaneous options
-;;(setq-default major-mode
-;;              (lambda () ; guess major mode from file name
-;;                (unless buffer-file-name
-;;                  (let ((buffer-file-name (buffer-name)))
-;;                    (set-auto-mode)))))
-;;(setq confirm-kill-emacs #'yes-or-no-p)
-;;(setq window-resize-pixelwise t)
-;;(setq frame-resize-pixelwise t)
-;;(save-place-mode t)
-;;(savehist-mode t)
-;;(recentf-mode t)
-;;(defalias 'yes-or-no-p #'y-or-n-p)
+;; packages to try
+;; 'magit
+;; 'corfu
+;; 'minions
+;; 'diff-hl
+;; 'vterm
+;; 'ag
+;; 'doom-modeline
+;; 'nerd-icons
+;; 'eglot
+
+;; Miscellaneous options
+(setq-default major-mode
+              (lambda () ; guess major mode from file name
+                (unless buffer-file-name
+                  (let ((buffer-file-name (buffer-name)))
+                    (set-auto-mode)))))
+(setq confirm-kill-emacs #'yes-or-no-p)
+(setq window-resize-pixelwise t)
+(setq frame-resize-pixelwise t)
+(save-place-mode t)
+(savehist-mode t)
+(recentf-mode t)
+(defalias 'yes-or-no-p #'y-or-n-p)
 
